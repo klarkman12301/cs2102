84225a404f34d4cac4b935f22746526ab9121892
klark <klarkman12301@gmail.com>
2022-02-08 14:40:55 +0600

    first commit

---
+++ src/com/company/Application.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Application {
+    class rooms {
+        String initial;
+        int synyp;
+        int ave_point;
+
+    }
+    class student{
+        String name_surname;
+        int id;
+        int vozrast;
+        int ocenki;
+
+
+    }
+    class lesson;
+        String name;
+
+
+}
